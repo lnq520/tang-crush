05f843bcac5c5c40a7a43cd2f59abdac002ba882
lnq520 <88776273+lnq520@users.noreply.github.com>
2021-08-11 18:31:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `T có chuyện này muốn hỏi m, m phải trả lời thật lòng á. Nếu không Tăng Thiên Thiên sé là của t`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Sau này lỡ thất nghiệp thì m có nuôi t hong???? 😙',
+    desc: 'M mà thoát ra đ trả lời thì có nghĩa là m thích t á. Suy nghĩ đi ',
+    btnYes: 'Nuôi chứ <33',
+    btnNo: 'Đéo nha :3',
+    question:'Sao m lại đồng ý nuôi t thế? <3',
+    btnReply: 'Trả lời t đi <3',
+    reply: 'M là đứa đáng yêu nhất mà t quen  <33333333',
+    mess: 'T biết mà 🥰. Hihi. M cũng đáng yêu vl 😘😘',
+    messDesc: 'M là bánh xe dự phòng của t nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
